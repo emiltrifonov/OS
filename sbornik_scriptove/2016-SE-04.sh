36968f0a43640ec398e79ffd731a9b3bf5e500e5
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-04-06 17:28:26 +0300

    Create 2016-SE-04.sh

---
+++ sbornik_scriptove/2016-SE-04.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $# -ne 2 ]]; then
+    echo "Script must take in exactly 2 arguments"
+    exit 1
+fi
+
+reg=^[1-9][0-9]*$
+
+if [[ ! $1 =~ $reg || ! $2 =~ $reg ]]; then
+    echo "Both arguments must be valid numbers"
+    exit 2
+fi
+
+mkdir -p ./a ./b ./c
+a="./a"; b="./b"; c="./c"
+
+find . -type f ! -name "$(basename $0)" | while IFS= read -r file; do
+
+    lines=$(wc -l < "$file")
+
+    if [[ $lines -lt $1 ]]; then
+        mv "$file" "${a}/$(basename "${file}")"
+    elif [[ $lines -le $2 ]]; then
+        mv "$file" "${b}/$(basename "${file}")"
+    else
+        mv "$file" "${c}/$(basename "${file}")"
+    fi
+
+done
+
+exit 0
