7462d282b371137cc00c322c52739c0148a70b69
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-06-14 12:56:38 +0300

    Update ls_one_arg.c

--- C/file io/ls_one_arg.c
+++ C/file io/ls_one_arg.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
         errx(2, "args pls");
     }
 
-    int rv = execlp("ls", "ls %s", argv[1], (char*)NULL);
+    int rv = execlp("ls", "ls", argv[1], (char*)NULL);
     if (rv < 0) {
         err(1, "couldnt exec");
     }
