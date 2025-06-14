0557b1db8b41bca2de55e08ca5e28686cf223737
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-06-14 23:44:35 +0300

    Update task6.c

--- C/processes/task6.c
+++ C/processes/task6.c
@@ -14,6 +14,10 @@ int main(int argc, char* argv[])
     if (argc != 2) { errx(1, "args pls"); }
 
     pid_t pid = fork();
+    if (pid < 0) {
+        err(2, "cant fork");
+    }
+    
     if (pid == 0) {
         //child
 
