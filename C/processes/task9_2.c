12ca111cc797ad49d2d23aa2a477955d4a56c62f
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-06-15 16:58:29 +0300

    Create task9_2.c

---
+++ C/processes/task9_2.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+    int fd = open("my_temp_file", O_WRONLY | O_TRUNC | O_CREAT, 0666);
+    if (fd < 0) { err(1, "cant open"); }
+
+    for (int i = 0; i < 8; i++) {
+        pid_t pid = fork();
+        if (pid < 0) { err(2, "cant fork"); }
+
+        if (pid == 0) {
+            if (write(1, "foo\n", 4) != 4) { err(3, "cant write"); }
+            exit(0);
+        }
+        else {
+            wait(0);
+            if (write(1, "bar\n", 4) != 4) { err(4, "cant write"); }
+        }
+    }
+
+    close(fd);
+
+    return 0;
+}
