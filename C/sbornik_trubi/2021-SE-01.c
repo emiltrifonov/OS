6c7d8ccf27d905d868f4997bad5eaa45934d857d
emiltrifonov <103438524+emiltrifonov@users.noreply.github.com>
2025-06-20 15:35:48 +0300

    Create 2021-SE-01.c

---
+++ C/sbornik_trubi/2021-SE-01.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <err.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <time.h>
+
+int main(int argc, char* argv[])
+{
+  
+}
